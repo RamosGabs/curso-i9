f5d36927218c53798773c304ee3428fc7890269d
RamosGa <50061914+RamosGa@users.noreply.github.com>
2021-08-07 22:34:10 -0300

    Criação do crud de cadastro com bootstrap

--- iniciando/src/App.js
+++ iniciando/src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import reactDom from 'react-dom';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App (props){
+  
+
+  const modificarNome = event  => {
+    console.log(event.target.value)
+  }
+
+
+
+  const criarComboBox = () => {
+    const opcoes = ["Biel", "Ramos"]
+    const criarComboOptions = opcoes.map( option => <option>{option}</option>)
+
+    return(
+      <select>
+        {criarComboOptions}
+      </select>
+    )
+  }
+
+  const meuComboBox = () => criarComboBox()
+
+  return(
+      <>
+        <input type="text" value={props.nome} onChange={modificarNome} placeholder={"Digite seu nome"}/> 
+        <h1>Seu Nome é: {props.nome}</h1>
+        <meuComboBox/>
+
+      </>
+      
+    )
 }
+
 
 export default App;
